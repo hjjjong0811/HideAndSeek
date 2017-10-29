7d2d21dd179d09c62fda63b7be569b887a8c09f1
hjjjong0811 <hjjjong0811@gmail.com>
2017-10-29 14:42:20 +0900

    Item DataEditor 제작함

---
+++ HideAndSeek_DataEdit/Assets/Script/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System;
+using System.IO;
+
+[Serializable]
+public class Item {
+    [SerializeField]
+    public int Key;
+    public String Name;
+    public String Info;
+    public String Img;
+}
