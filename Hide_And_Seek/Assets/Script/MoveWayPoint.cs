a2414a46ff1fa823b56e95febe8e66d55fafdbd5
hjjjong0811 <hjjjong0811@gmail.com>
2017-11-11 16:24:45 +0900

    movewayPoint 코드정리 + 애니메잉션 뒤돌기 수정

--- Hide_And_Seek/Assets/Script/MoveWayPoint.cs
+++ Hide_And_Seek/Assets/Script/MoveWayPoint.cs
@@ -9,24 +9,24 @@ public class MoveWayPoint : MonoBehaviour {
     public bool isCharacter;    //isCharacter?, is Setted at Prefab inspector, And if true set Animator
     public Animator animator;
 
-    public Vector2[] wayPoint;      //이동포인트
+    public Vector2[] wayPoint = null;      //이동포인트
     public int pointIndex;
     public float speed;
 
 	// Use this for initialization
 	void Start () {
-        if(isCharacter) animator = GetComponent<Animator>();
-        pointIndex = 0;
-
-        //test
-        move(new Vector2[] {new Vector2(0.8f,-1.68f), new Vector2(2.69f,-0.1f) }, Speed_walk);
-	}
-
-    private void Update() {
-        if (isCharacter) {  //캐릭터인경우 애니메이션 초기화
+        //캐릭터인경우 애니메이션 초기화
+        if (isCharacter) {
+            animator = GetComponent<Animator>();
             animator.SetInteger("State", Ani_Idle);
             animator.speed = Speed_walk;
         }
+            pointIndex = 0;
+        speed = Speed_walk;
+        
+	}
+
+    private void Update() {
         //목표 웨이포인트 존재시
         if(wayPoint != null) {
             //이동
@@ -40,6 +40,11 @@ public class MoveWayPoint : MonoBehaviour {
                 if (pointIndex == wayPoint.Length) {
                     wayPoint = null;    //이동종료
                     pointIndex = 0;
+
+                    if (isCharacter) {
+                        animator.SetInteger("State", Ani_Idle);
+                        animator.speed = Speed_walk;
+                    }
                 }
             }
 
@@ -47,26 +52,28 @@ public class MoveWayPoint : MonoBehaviour {
     }
 
     private void setAni(Vector2 target) {
-        animator.SetBool("Back", false);
-
         //속도별 애니메이션
-        if (speed == Speed_run) {
+        if (speed >= Speed_run) {
             animator.SetInteger("State", Ani_Run);
             animator.speed = Speed_run;
         }else { animator.SetInteger("State", Ani_Walk); }
 
         //수평 이동값 계산
-        float horizontal = (target - (Vector2)transform.position).normalized.x;
+        Vector2 direction = (target - (Vector2)transform.position).normalized;
+
         //위로가는경우 뒷모습
-        if (horizontal < 0.4 && horizontal > -0.4) {
+        if (direction.y > 0 && direction.x < 0.4 && direction.x > -0.4) {
             animator.SetBool("Back", true);
+        } else {
+            animator.SetBool("Back", false);
         }
+
         //좌우반전
-        if (horizontal > 0) {
+        if (direction.x > 0) {
             Vector3 scale = transform.localScale;
             scale.x = -Mathf.Abs(scale.x);
             transform.localScale = scale;
-        } else if (horizontal < 0) {
+        } else if (direction.x < 0) {
             Vector3 scale = transform.localScale;
             scale.x = Mathf.Abs(scale.x);
             transform.localScale = scale;
