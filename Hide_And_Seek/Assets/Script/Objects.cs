2c13638fdff1c0e28fc7a1b19b5da1016f6cd99b
HOB <hobin1019@naver.com>
2017-11-22 17:08:51 +0900

    스크립트

--- Hide_And_Seek/Assets/Script/Objects.cs
+++ Hide_And_Seek/Assets/Script/Objects.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Object_State
+{
+    too_far = -1,
+    Object_back = 0,
+    Object_front = 1
+}
 public class Objects : MonoBehaviour
 {
     string[] object_sorting = new string[] { "Object_back", "Object_front" };
-    GameObject[] child_objects = new GameObject[] { };
-    int child_objects_num;
+    List<SpriteRenderer> sr_list = new List<SpriteRenderer>();
 
     // Use this for initialization
     void Start()
     {
-        child_objects_num = 0;
+        Transform[] child_objects = GetComponentsInChildren<Transform>();
+        foreach (Transform t in child_objects)
+        {
+            if (t.gameObject.GetComponent<SpriteRenderer>() != null)
+            {
+                sr_list.Add(t.gameObject.GetComponent<SpriteRenderer>());
+            }
+        }
     }
 
     // Update is called once per frame
@@ -20,15 +32,19 @@ public class Objects : MonoBehaviour
         check_back_front();
     }
 
+    void change_childs_back_fround(string order)
+    {
+        foreach (SpriteRenderer sr in sr_list)
+        {
+            sr.sortingLayerName = order;
+        }
+    }
+
     void check_back_front()
     {
-        if (this.transform.position.y > Player.Player_obj.GetComponent<Player>().get_player_pos().y)
-        {
-            this.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = object_sorting[0];
-        }
-        else
-        {
-            this.gameObject.GetComponent<SpriteRenderer>().sortingLayerName = object_sorting[1];
-        }
+        Object_State obj_loc = Player.check_up_down(this.gameObject.name);
+
+        if (obj_loc == Object_State.too_far) return;
+        change_childs_back_fround(object_sorting[(int)obj_loc]);
     }
 }
