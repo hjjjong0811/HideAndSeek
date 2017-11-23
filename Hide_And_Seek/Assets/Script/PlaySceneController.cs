4f65ab902210b1929de598eda0cf79485feff0f1
hjjjong0811 <hjjjong0811@gmail.com>
2017-11-23 21:50:19 +0900

    튜토리얼 조금수정, 비비큐씬수정(material 적용, 카메라)

--- Hide_And_Seek/Assets/Script/PlaySceneController.cs
+++ Hide_And_Seek/Assets/Script/PlaySceneController.cs
@@ -15,6 +15,10 @@ public class PlaySceneController : MonoBehaviour {
     
     public void setScene(PlayScene.numScene n) {
         if (n == PlayScene.numScene.tutorial) play = playTutorial;
+        else if (n == PlayScene.numScene.hide_1_end) play = playHide1;
+        else if (n == PlayScene.numScene.hide_2_ready) play = playHide2Ready;
+        else if (n == PlayScene.numScene.ringPhone) play = ringPhone;
+        else if (n == PlayScene.numScene.ending_exit) play = end_1;
     }
     private void Awake() {
         DontDestroyOnLoad(this.gameObject);
@@ -26,7 +30,7 @@ public class PlaySceneController : MonoBehaviour {
     private IEnumerator playTutorial() {
         //씬 로드
         SceneManager.LoadScene("S_BBQ");
-        yield return new WaitForSeconds(0.01f);
+        yield return new WaitForSeconds(2f);
 
         //대사 진행
         //ScriptManager 펜션도착 대사 진행 요청
@@ -41,11 +45,19 @@ public class PlaySceneController : MonoBehaviour {
         GameObject ch_suun = Instantiate(pre_ch_su);
         GameObject ch_pension = Instantiate(pre_ch_ps);
         GameObject light_obj = Instantiate(pre_light_direc);
-        yield return new WaitForSeconds(0.01f);                 //instantiate후 지연
+        yield return new WaitForSeconds(0.001f);                 //instantiate후 지연
+
+        //위치 설정
+        ch_main.transform.position = new Vector3(3.25f, -1.64f, 0);
+        ch_habin.transform.position = new Vector3(-0.8f, 0.35f, 0);
+        ch_hyojung.transform.position = new Vector3(0.1f, -1.93f, 0);
+        ch_suun.transform.position = new Vector3(1.873192f, 0.2435494f, 0);
+        ch_jungyeon.transform.position = new Vector3(-1.382864f, -1.384479f, 0);
+        ch_pension.transform.position = new Vector3(-3.23f, -0.12f, 0);
 
         Light_Directional light = light_obj.GetComponent<Light_Directional>();
         light.fadeIn(2.0f);   //페이드인
-        yield return new WaitForSeconds(1f);    //대사 전 딜레이
+        yield return new WaitForSeconds(3f);    //대사 전 딜레이
 
         //ScriptManager 바베큐장 대사 진행 요청
 
@@ -59,23 +71,20 @@ public class PlaySceneController : MonoBehaviour {
         myChar.Add(ch_jungyeon.GetComponent<MoveWayPoint>());
         myChar.Add(ch_suun.GetComponent<MoveWayPoint>());
 
-        for (int i = 0; i < myChar.Count; i++) {
-            myChar[i].move(new Vector2[] { new Vector2(1, -1) }, MoveWayPoint.Speed_run);
-        }
-        //모든 캐릭터 이동 끝나는지 확인
-        int check = 0;
-        while(check != myChar.Count) {
-            check = 0;
-            for (int i = 0; i < myChar.Count; i++) {
-                if (myChar[i].isIdle()) {
-                    check++;
-                }
-            }
-            yield return new WaitForSeconds(0.5f);  //0.5초마다 확인
-        }
-        myChar.Clear();
-        myChar = null;
-        light.fadeOut(3.0f);
+        ch_suun.GetComponent<MoveWayPoint>().move(
+            new Vector2[] { new Vector2(-0.19f, 1.27f), new Vector2(-20, -1) }, MoveWayPoint.Speed_walk);
+        ch_habin.GetComponent<MoveWayPoint>().move(
+            new Vector2[] { new Vector2(-2.68f, 1.1f), new Vector2(-20, -1) }, MoveWayPoint.Speed_walk);
+        ch_main.GetComponent<MoveWayPoint>().move(
+            new Vector2[] { new Vector2(0.86f, -3.14f), new Vector2(-20, -1) }, MoveWayPoint.Speed_walk);
+        ch_hyojung.GetComponent<MoveWayPoint>().move(
+            new Vector2[] {new Vector2(-20, -1) }, MoveWayPoint.Speed_walk);
+        ch_jungyeon.GetComponent<MoveWayPoint>().move(
+            new Vector2[] { new Vector2(-20, -1) }, MoveWayPoint.Speed_walk);
+
+        yield return new WaitForSeconds(3f);    //3초지연
+        light.fadeOut(2.0f);
+        yield return new WaitForSeconds(2f);    //2초지연
 
         //숨바곡질 시작 씬 로드
         SceneManager.LoadScene("1_Hall");
@@ -116,7 +125,7 @@ public class PlaySceneController : MonoBehaviour {
             myChar[i].move(new Vector2[] { new Vector2(0.5f, 0) }, MoveWayPoint.Speed_run);
         }
         //모든 캐릭터 이동 끝나는지 확인
-        check = 0;
+        int check = 0;
         while (check != myChar.Count) {
             check = 0;
             for (int i = 0; i < myChar.Count; i++) {
@@ -137,5 +146,20 @@ public class PlaySceneController : MonoBehaviour {
 
         yield break;
     }
-    
+
+    private IEnumerator playHide1() {
+        yield break;
+    }
+
+    private IEnumerator playHide2Ready() {
+        yield break;
+    }
+
+    private IEnumerator ringPhone() {
+        yield break;
+    }
+
+    private IEnumerator end_1() {
+        yield break;
+    }
 }
