d3bae5ab95812411c88e5a0ed0d21edfb2f3c49f
HOB <hobin1019@naver.com>
2017-11-24 21:05:18 +0900

    상속관계완성

--- Hide_And_Seek/Assets/Script/Object_Manager.cs
+++ Hide_And_Seek/Assets/Script/Object_Manager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum Object_State
 {
@@ -9,37 +10,62 @@ public enum Object_State
     Object_front = 1
 }
 public class Object_Manager{
-
-	
 }
 
 public interface IObject
 {
+    void object_action();
     void for_start();
     void for_update();
 }
 public class Portal : IObject
 {
-    int key_num;
-    Room destination = Room.Living_1;
+    int key_num=0;
+    string scene_name1, scene_name2;
 
-    public Portal(int key_num)
+    public Portal(int key_num, Room r1, Room r2)
     {
         this.key_num = key_num;
+        scene_name1 = Scene_Manager.scene_name[(int)r1];
+        scene_name2 = Scene_Manager.scene_name[(int)r2];
     }
 
+    public void object_action(){
+
+    }
     public void for_start()
     {
     }
 
     public void for_update()
     {
+        testing();//test
+    }
+
+    public int get_key()
+    {
+        return key_num;
+    }
+
+    void testing()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (SceneManager.GetActiveScene().name == scene_name1)
+            {
+                SceneManager.LoadScene(scene_name2);
+            }
+            else
+            {
+                SceneManager.LoadScene(scene_name1);
+            }
+        }
     }
 }
 public class Thing : IObject
 {
     GameObject obj;
-    int key_num = 1;
+    int key_num=0;
     static string[] object_sorting = new string[] { "Object_back", "Object_front" };
     List<SpriteRenderer> sr_list = new List<SpriteRenderer>();
 
@@ -49,11 +75,29 @@ public class Thing : IObject
         this.obj = obj;
     }
 
+    //player에서 호출하는 함수
+    public void object_action(){
+    }
+    //start()용 함수
+    public void for_start()
+    {
+        Transform[] child_objects = obj.GetComponentsInChildren<Transform>();
+        foreach (Transform t in child_objects)
+        {
+            if (t.gameObject.GetComponent<SpriteRenderer>() != null)
+            {
+                sr_list.Add(t.gameObject.GetComponent<SpriteRenderer>());
+            }
+        }
+    }
     //update()용 함수
     public void for_update()
     {
         change_childs();
     }
+
+
+
     void change_childs()
     {
         Object_State obj_loc = Player.check_up_down(obj.name);
@@ -66,19 +110,6 @@ public class Thing : IObject
         foreach (SpriteRenderer sr in sr_list)
         {
             sr.sortingLayerName = order;
-        }
-    }
-
-    //start()용 함수
-    public void for_start()
-    {
-        Transform[] child_objects = obj.GetComponentsInChildren<Transform>();
-        foreach (Transform t in child_objects)
-        {
-            if (t.gameObject.GetComponent<SpriteRenderer>() != null)
-            {
-                sr_list.Add(t.gameObject.GetComponent<SpriteRenderer>());
-            }
         }
     }
 }
--- Hide_And_Seek/Assets/Script/Objects.cs
+++ Hide_And_Seek/Assets/Script/Objects.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Objects : MonoBehaviour
 {
+    public int _key_num;
+    public bool _t_thing_f_portal = false;//오브젝트 구분용 (true : 씬이동용 포탈 / false : 스크립트용 물건)
+    IObject _obj;
 
     // Use this for initialization
     void Start()
     {
+        if (_t_thing_f_portal)
+        {
+            _obj = new Thing(_key_num, this.gameObject);
+            _obj.for_start();
+        }
+        else
+        {
+            _obj = Scene_Manager.getInstance()._get_portal(_key_num);
+            _obj.for_start();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        _obj.for_update();
     }
 
 }
