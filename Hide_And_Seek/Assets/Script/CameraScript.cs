7af7616164abf1a3c82582495a39780fec50c3ff
hjjjong0811 <hjjjong0811@gmail.com>
2017-11-29 02:12:15 +0900

    카메라스크립트수정

--- Hide_And_Seek/Assets/Script/CameraScript.cs
+++ Hide_And_Seek/Assets/Script/CameraScript.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraScript : MonoBehaviour {
-    private enum effect {shake = 0};
-
     private Camera MyCamera;
     private Transform Player;
     private bool isEffect;
@@ -20,6 +18,10 @@ public class CameraScript : MonoBehaviour {
         isEffect = false;
         MyCamera.backgroundColor = Color.black;
         MyCamera.orthographicSize = 4;
+    }
+
+    public void linkUser(GameObject go) {
+        Player = go.transform;
     }
 
     public void zoom(Vector2 position, float size) {
@@ -42,7 +44,8 @@ public class CameraScript : MonoBehaviour {
     }
 
     public void shakeCamera() {
-        StartCoroutine(myCoroutine(effect.shake));
+        Debug.Log("shake Start");
+        StartCoroutine(shake());
     }
 
     private IEnumerator zoomSlow(Vector2 position, float size) {
@@ -61,15 +64,13 @@ public class CameraScript : MonoBehaviour {
         yield break;
     }
 
-    private IEnumerator myCoroutine(effect what) {
-        if (Player == null) yield break;
+    private IEnumerator shake() {
+        Vector2 v = transform.position;
         isEffect = true;
-        if(what == effect.shake) {
-            for (int i = 0; i < 10; i++) {
-                this.transform.position = new Vector3(
-                    Player.position.x + (Random.value - 0.5f)/i, Player.position.y + (Random.value - 0.5f)/i, -10);
-                yield return new WaitForSeconds(0.05f);
-            }
+        for (int i = 0; i < 10; i++) {
+            this.transform.position = new Vector3(
+                v.x + (Random.value - 0.5f)/i, v.y + (Random.value - 0.5f)/i, -10);
+            yield return new WaitForSeconds(0.05f);
         }
         isEffect = false;
 
