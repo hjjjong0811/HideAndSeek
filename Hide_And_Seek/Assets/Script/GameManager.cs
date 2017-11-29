f0c3ff8b7362f3f4da25813791da414b8bc3cb8f
jungwon7769 <jungwon7769@naver.com>
2017-11-29 12:35:41 +0900

    game조금수정..중간커밋!

--- Hide_And_Seek/Assets/Script/GameManager.cs
+++ Hide_And_Seek/Assets/Script/GameManager.cs
@@ -14,7 +14,7 @@ public class GameManager
 
     public static int MainChapter; // 전체 메인 에피소드 관리
 
-    public int[] EndScene; // 씬 종료 체크 배열
+    public int[] EndScene;
     public int[] FindCharacter; // 친구찾기 배열
     public int[] DeadCharacter; // 죽은 친구 배열
     public int[] MeetCharacter; // 만난 친구 배열
@@ -34,12 +34,13 @@ public class GameManager
     public int CorrectPassword; // 비밀번호 일치 여부
 
     public bool isScenePlay;    //현정추가! 컷씬플레이중인지 여부
+ 
 
     private GameManager()
     {
         MainChapter = -1;
 
-        EndScene = new int[2]{ 0,0 };
+        EndScene = new int[15] {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
         FindCharacter = new int[4] { 0, 0, 0, 0 };
         DeadCharacter = new int[4] { 0, 0, 0, 0 };
         MeetCharacter = new int[4] { 0, 0, 0, 0 };
@@ -82,48 +83,48 @@ public class GameManager
             //SetMainChapter(0);
             //isScenePlay = true;
             //PlayScene.getInstance().playScene(PlayScene.numScene.tutorial);
+            
+
         }
+
         /*M=0 튜토리얼*/
-        if (MainChapter == 0 && !isScenePlay) {
+        if (MainChapter == 0 && !isScenePlay && EndScene[0] == 1) 
             SetMainChapter(1);
-        }
+
+
         /*M=1 1차숨바꼭질(친구찾기)*/
-        else if (MainChapter == 1 && CheckArray(FindCharacter, 4)) { // 숨바꼭질로 친구들 다 찾았을때
+        else if (MainChapter == 1 && CheckArray(FindCharacter, 4))  // 숨바꼭질로 친구들 다 찾았을때
             //현정추가 컷씬시작
             SetMainChapter(2);
-            isScenePlay = true;
-            PlayScene.getInstance().playScene(PlayScene.numScene.hide_1_end);
-        }
+ 
         //현정추가 컷씬종료(컷씬 시작 전,종료시 챕터+1 해주길바람)
-        else if (MainChapter == 2 && !isScenePlay) {
+        else if (MainChapter == 2 && !isScenePlay && EndScene[1] == 1) 
             SetMainChapter(3);
-        }
+        
         /*M=2 소금물없어서 소주찾으러*/
-        else if (MainChapter == 3 && Soju == 1) { // 소주 획득시
+        else if (MainChapter == 3 && Soju == 1 && EndScene[1] == 1) 
             SetMainChapter(4);
-            isScenePlay = true;
-            PlayScene.getInstance().playScene(PlayScene.numScene.hide_2_ready);
-        }
+     
+
         /*M=3 준비물 모두 모아서 장롱씬*/
         else if (MainChapter == 4 && !isScenePlay) // 2차 장롱씬 종료시
             SetMainChapter(5);
 
         /*M=4 시작 정연이 5번누르면 엔딩 또는 효정이 확인 */
-        else if (MainChapter == 5 && MeetCharacter[1] == 5) { // 정연엔딩
+        else if (MainChapter == 5 && MeetCharacter[1] == 5)
             SetMainChapter(-2);
-            isScenePlay = true;
-            EndingPlay(-2);
-            //PlayScene
-        } else if (MainChapter == 5 && DeadCharacter[0] == 1)
+
+        else if (MainChapter == 5 && DeadCharacter[0] == 1)
             SetMainChapter(6);
 
         /*M=5 소금을 얻어야 겠다*///하빈만난부분
         else if (MainChapter == 6 && MeetCharacter[2] == 1)
             SetMainChapter(7);
 
-        else if (MainChapter == 7 && Salt == 1) {
+        else if (MainChapter == 7 && Salt == 1)
             SetMainChapter(8);
-        } else if (MainChapter == 8 && CheckArray(FindJeongyeon, 8))
+        
+        else if (MainChapter == 8 && CheckArray(FindJeongyeon, 8))
             SetMainChapter(9);
 
         //하빈gg
@@ -154,13 +155,12 @@ public class GameManager
 
 
         /*M= 11 서운 죽음*/
-        else if (MainChapter == 12 && DeadCharacter[3] == 1) {
+        else if (MainChapter == 12 && DeadCharacter[3] == 1)
             SetMainChapter(13);
-            isScenePlay = true;
-            PlayScene.getInstance().playScene(PlayScene.numScene.ringPhone);
-        } else if (MainChapter == 13 && !isScenePlay) {
+
+        else if (MainChapter == 13 && !isScenePlay) 
             SetMainChapter(14);
-        }
+ 
         //띠벽지애기그림
         else if (MainChapter == 14 && Wallpaper == 1)
             SetMainChapter(15); //이제 인화실발견가능
@@ -174,15 +174,41 @@ public class GameManager
             SetMainChapter(17); //이제 지하실의 바깥통로 사용가능
 
         //통로 문 염
-        else if (MainChapter == 17 && CorrectPassword == 1) {
+        else if (MainChapter == 17 && CorrectPassword == 1)
             SetMainChapter(18);
-            isScenePlay = true;
-            EndingPlay(-3);
+        
+
+    }
+
+
+    public void StoryPlayScene()
+    {
+        int Chapter = GetMainChapter();
+
+        switch(Chapter)
+        {
+            case 0: if (!isScenePlay && EndScene[0] == 0)
+                { isScenePlay = true;
+                  EndScene[0] = 1;
+                  ScenePlay(0); }
+            break;
+                
+            case 2: if (!isScenePlay && EndScene[1] == 0) { isScenePlay = true; EndScene[1] = 1; ScenePlay(1); } break;
+            case 3: if (!isScenePlay && EndScene[2] == 0) { isScenePlay = true; EndScene[2] = 1; ScenePlay(2); } break;
+
+            
+
+
         }
 
     }
 
-    public void EndingPlay(int EndingNum) // 엔딩번호 전달받는 경우 playScene 호출 -> 해당스토리 재생
+
+    /// <summary>
+    /// GameManager.getInstance().ScenePlay(int EndingNum)
+    /// </summary>
+    /// <param name="EndingNum"></param>
+    public void ScenePlay(int EndingNum) // 엔딩번호 전달받는 경우 playScene 호출 -> 해당스토리 재생
     {
         switch(EndingNum)
         {
@@ -208,7 +234,6 @@ public class GameManager
 
     }
 
-
     public bool CheckArray(int[] TestArray, int ArrayNum)
     {
         int Num = 0;
@@ -231,7 +256,7 @@ public class GameManager
     {
         MainChapter = -1;
 
-        EndScene = new int[2] { 0, 0 };
+        EndScene = new int[15] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         FindCharacter = new int[4] { 0, 0, 0, 0 };
         DeadCharacter = new int[4] { 0, 0, 0, 0 };
         MeetCharacter = new int[4] { 0, 0, 0, 0 };
--- Hide_And_Seek/Assets/Script/SoundUIManager.cs
+++ Hide_And_Seek/Assets/Script/SoundUIManager.cs
@@ -1,13 +1,77 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundUIManager : MonoBehaviour {
 
-    //public AudioSource audioSource;
-    //public Toggle SoundController;
+    public AudioSource BGM;
+    public AudioSource Effect;
+    public AudioSource FootStep;
+   
+    public Toggle BGMtoggle;
+    public Toggle EffectToggle;
 
-    public void Btn_SoundOff()
+
+    public void Awake()
+    {
+        BGM = GetComponent<AudioSource>();
+        Effect = GetComponent<AudioSource>();
+        FootStep = GetComponent<AudioSource>();
+    }
+
+    public void Start()
+    {
+        audioSource.Play();
+    
+    }
+
+    public void SaveData()
+    {
+        SceneManager.LoadScene("save");
+
+    }
+
+
+    public void Btn_Setting()
+    {
+        SettingPanel.SetActive(true);
+
+    }
+
+    public void Btn_SettingDelete()
+    {
+        SettingPanel.SetActive(false);
+    }
+
+    public void Btn_Developer()
+    {
+        DeveloperPanel.SetActive(true);
+    }
+
+    public void Btn_DeveloperDelete()
+    {
+        DeveloperPanel.SetActive(false);
+    }
+
+    public void SoundOnOff()
+    {
+        if (SoundControll.isOn.Equals(true))
+        {
+
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+
+
+
+
+public void Btn_SoundOff()
     {
         GameObject.Destroy(GameObject.Find("Sound"));
     }
