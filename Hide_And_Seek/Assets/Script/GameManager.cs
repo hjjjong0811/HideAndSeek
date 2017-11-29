4b30797846d8d22aa5cce51ce7b1963487a73f80
jungwon7769 <jungwon7769@naver.com>
2017-11-29 16:41:53 +0900

    StoryPlayScene()추가

--- Hide_And_Seek/Assets/Script/GameManager.cs
+++ Hide_And_Seek/Assets/Script/GameManager.cs
@@ -63,6 +63,11 @@ public class GameManager
         return instance;
     }
 
+    public void Update()
+    {
+        StoryPlayScene();
+    }
+
     public void GetItem(int ItemKey) // 스토리에 관련된 아이템 얻을때
     {
         switch (ItemKey)
@@ -76,76 +81,79 @@ public class GameManager
     
     public void CheckMainChapter() // 챕터넘어갈 이벤트 만족했는지 확인
     {
+
+
         //현정 컷신으로인한 챕터 번호수정있음! 주석추가있음
         //현정 추가
-        if(MainChapter == -1) {
+        if (MainChapter == -1)
+        {
             //테스트해야하는데 자꾸 튜토링러 진행돼서 잠깐 아래 세줄 주석처리함;;
             //SetMainChapter(0);
             //isScenePlay = true;
             //PlayScene.getInstance().playScene(PlayScene.numScene.tutorial);
-            
+
 
         }
 
-        /*M=0 튜토리얼*/
-        if (MainChapter == 0 && !isScenePlay && EndScene[0] == 1) 
+        //튜토리얼씬 종료시 -> 1로간다.
+        else if (MainChapter == 0 && !isScenePlay && EndScene[0] == 1)
             SetMainChapter(1);
 
-
-        /*M=1 1차숨바꼭질(친구찾기)*/
-        else if (MainChapter == 1 && CheckArray(FindCharacter, 4))  // 숨바꼭질로 친구들 다 찾았을때
-            //현정추가 컷씬시작
+        //숨은 친구들 다 찾으면 -> 2로
+        else if (MainChapter == 1 && CheckArray(FindCharacter, 4))
             SetMainChapter(2);
- 
-        //현정추가 컷씬종료(컷씬 시작 전,종료시 챕터+1 해주길바람)
-        else if (MainChapter == 2 && !isScenePlay && EndScene[1] == 1) 
-            SetMainChapter(3);
-        
-        /*M=2 소금물없어서 소주찾으러*/
-        else if (MainChapter == 3 && Soju == 1 && EndScene[1] == 1) 
-            SetMainChapter(4);
-     
 
-        /*M=3 준비물 모두 모아서 장롱씬*/
-        else if (MainChapter == 4 && !isScenePlay) // 2차 장롱씬 종료시
+        // hide_1_end 씬종료되고, 소주 얻었으면 ->3
+        else if (MainChapter == 2 && !isScenePlay && EndScene[1] == 1 && Soju == 1)
+            SetMainChapter(3);
+
+        // hide_2_ready 씬 종료되면(준비끝) ->4
+        else if (MainChapter == 3 && !isScenePlay && EndScene[2] == 1)
+            SetMainChapter(4);
+
+        // 게임시작 효정이 만나면 -> 5
+        else if (MainChapter == 4 && DeadCharacter[0] == 1)
             SetMainChapter(5);
 
-        /*M=4 시작 정연이 5번누르면 엔딩 또는 효정이 확인 */
-        else if (MainChapter == 5 && MeetCharacter[1] == 5)
+        // 정연이 5번 -> -2(엔딩)
+        else if (MainChapter == 4 && MeetCharacter[1] == 5)
             SetMainChapter(-2);
 
-        else if (MainChapter == 5 && DeadCharacter[0] == 1)
+        // 하빈이 만나면 ->6
+        else if (MainChapter == 5 && MeetCharacter[2] == 1)
             SetMainChapter(6);
 
-        /*M=5 소금을 얻어야 겠다*///하빈만난부분
-        else if (MainChapter == 6 && MeetCharacter[2] == 1)
+        // 소금있으면 ->7
+        else if (MainChapter == 6 && !isScenePlay && EndScene[3] == 1 && Salt == 1)
             SetMainChapter(7);
 
-        else if (MainChapter == 7 && Salt == 1)
+        // 정연이 찾으면 ->8
+        else if (MainChapter == 7 && !isScenePlay && EndScene[4] == 1 && CheckArray(FindJeongyeon, 8))
             SetMainChapter(8);
-        
-        else if (MainChapter == 8 && CheckArray(FindJeongyeon, 8))
-            SetMainChapter(9);
 
-        //하빈gg
-        else if (MainChapter >= 9 && SaltyWater == 1)
+        // 소금물 보유시 챕터 증가 -> 8+1
+        else if (MainChapter >= 8 && SaltyWater == 1)
         {
-            if(CheckOverlap[0] == 0)
-            {       
+            if (CheckOverlap[0] == 0)
+            {
                 CheckOverlap[0] = 1;
                 SetMainChapter(MainChapter + 1);
             }
-
         }
 
-        else if (MainChapter >= 9 && BreakDisplay == 1) {
+        // 장식장 깨져있으면 챕터증가 -> 8+1
+        else if (MainChapter >= 8 && !isScenePlay && EndScene[8] == 1 && BreakDisplay == 1)
+        {
             if (CheckOverlap[1] == 0)
             {
                 CheckOverlap[1] = 1;
                 SetMainChapter(MainChapter + 1);
             }
+        }
 
-        } else if (MainChapter >= 9 && DeadCharacter[2] == 1) {
+        // 세탁기 소리 씬재생후 정연이 죽음 확인-> 8+1
+        else if (MainChapter >= 8 && DeadCharacter[2] == 1 && !isScenePlay && EndScene[10] == 1)
+        {
             if (CheckOverlap[2] == 0)
             {
                 CheckOverlap[2] = 1;
@@ -153,30 +161,26 @@ public class GameManager
             }
         }
 
+        // 씬종료후 서운이 죽음 확인하면 -> 12
+        else if (MainChapter == 11 && DeadCharacter[3] == 1 && !isScenePlay && EndScene[11] == 1)
+            SetMainChapter(12);
 
-        /*M= 11 서운 죽음*/
-        else if (MainChapter == 12 && DeadCharacter[3] == 1)
+        // 띠벽지 확인하면 ->13
+        else if (MainChapter == 12 && Wallpaper == 1)
             SetMainChapter(13);
 
-        else if (MainChapter == 13 && !isScenePlay) 
+        // 집구조도 확인가능 -> 14
+        else if (MainChapter == 13 && HomeConstruct == 1)
             SetMainChapter(14);
- 
-        //띠벽지애기그림
-        else if (MainChapter == 14 && Wallpaper == 1)
-            SetMainChapter(15); //이제 인화실발견가능
 
-        //집구조도확인
-        else if (MainChapter == 15 && HomeConstruct == 1)
-            SetMainChapter(16); //이제 지하실 가능
+        // 지하실 열쇠 -> 15
+        else if (MainChapter == 14 && GroundKey == 1)
+            SetMainChapter(15);
 
-        //지하실+열쇠사용
-        else if (MainChapter == 16 && GroundKey == 1)
-            SetMainChapter(17); //이제 지하실의 바깥통로 사용가능
+        // 비밀번호 입력후 일치 -> 16(엔딩)
+        else if (MainChapter == 15 && CorrectPassword == 1)
+            SetMainChapter(16);
 
-        //통로 문 염
-        else if (MainChapter == 17 && CorrectPassword == 1)
-            SetMainChapter(18);
-        
 
     }
 
@@ -187,16 +191,42 @@ public class GameManager
 
         switch(Chapter)
         {
-            case 0: if (!isScenePlay && EndScene[0] == 0)
-                { isScenePlay = true;
-                  EndScene[0] = 1;
-                  ScenePlay(0); }
-            break;
+            // 튜토리얼일때 튜토리얼 씬진행
+            case 0: if (!isScenePlay && EndScene[0] == 0) { EndScene[0] = 1; isScenePlay = true; ScenePlay(0); } break;
                 
-            case 2: if (!isScenePlay && EndScene[1] == 0) { isScenePlay = true; EndScene[1] = 1; ScenePlay(1); } break;
-            case 3: if (!isScenePlay && EndScene[2] == 0) { isScenePlay = true; EndScene[2] = 1; ScenePlay(2); } break;
+            // 친구들 다 찾은 챕터. hide_1_end 씬 진행
+            case 2: if (!isScenePlay && EndScene[1] == 0) { EndScene[1] = 1; isScenePlay = true; ScenePlay(1); } break;
 
-            
+            // 소주 찾은 챕터. hide_2_ready 진행
+            case 3: if (!isScenePlay && EndScene[2] == 0) { EndScene[2] = 1; isScenePlay = true; ScenePlay(2); } break;
+
+                //하빈이 만난 챕터. 소금이 없음 씬 진행
+            case 6: if(!isScenePlay && EndScene[3] == 0) { EndScene[3] = 1; isScenePlay = true; ScenePlay(4); } break;
+                
+                // 소금이 있음. 정연이 찾아오라는 씬진행
+            case 7: if(!isScenePlay && EndScene[4] == 0) { EndScene[4] = 1; isScenePlay = true; ScenePlay(5); } break;
+
+
+            // 방 다 뒤져서 정연이없는상태임
+            case 8: case 9: case 10:
+                //정연이 없네 씬
+                if (!isScenePlay && EndScene[5] == 0) { EndScene[5] = 1; isScenePlay = true; ScenePlay(6); }
+                
+                //하빈이도 없네
+                else if (!isScenePlay && EndScene[6] == 0) { EndScene[6] = 1; isScenePlay = true; ScenePlay(7); }
+
+                //하빈이 죽었자나?!씬 
+                else if (!isScenePlay && EndScene[7] == 0) { EndScene[7] = 1; isScenePlay = true; ScenePlay(8); }
+
+                //장식장 뿌심
+                else if(!isScenePlay && EndScene[8] == 0) { EndScene[8] = 1; isScenePlay = true; ScenePlay(9); }
+
+                //장롱에 숨는씬
+                else if(!isScenePlay && EndScene[9] == 0) { EndScene[9] = 1; isScenePlay = true; ScenePlay(10); }
+                break;
+
+                // 장롱에 숨은 상태. 정연이 주거따(세탁기 씬)
+            case 11: if (!isScenePlay && EndScene[10] == 0) { EndScene[10] = 1; isScenePlay = true; ScenePlay(11); } break;
 
 
         }
