3f2c9188e244e718af8af107507f299e48362c24
hjjjong0811 <hjjjong0811@gmail.com>
2017-12-05 16:44:52 +0900

    옵젝수정+조건추가, 스크립트추가, 패스워드창 여러개열수있던거수정

--- Hide_And_Seek/Assets/Script/PasswordUIManager.cs
+++ Hide_And_Seek/Assets/Script/PasswordUIManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class PasswordUIManager {
+    public delegate void wake();
+    
     private GameObject window;
     private Text txtPassword;
     private Button []btnNum;
@@ -16,8 +18,15 @@ public class PasswordUIManager {
         password = pPassword;
 
         GameObject pre = Resources.Load("Prefabs/Canvas_Password") as GameObject;
+        pre.transform.GetChild(0).gameObject.SetActive(false);
         window = MonoBehaviour.Instantiate(pre);
+        PasswordUI_wait wait = window.AddComponent<PasswordUI_wait>();
+        wait.StartCoroutine(wait.waitScript(setUI));
+    }
 
+    public void setUI() {
+        ScriptManager.getInstance().isPlaying = true;
+        window.transform.GetChild(0).gameObject.SetActive(true);
         txtPassword = window.transform.GetChild(0).GetChild(0).GetComponent<Text>();
         txtPassword.text = "";
 
@@ -25,7 +34,7 @@ public class PasswordUIManager {
         Transform pnlBtn = window.transform.GetChild(0).GetChild(1);
         for (int i = 1; i <= 9; i++) {
             int n = i;
-            btnNum[n] = pnlBtn.GetChild(n-1).GetComponent<Button>();
+            btnNum[n] = pnlBtn.GetChild(n - 1).GetComponent<Button>();
             btnNum[n].onClick.AddListener(() => buttonClick(n));
         }
         btnNum[0] = pnlBtn.GetChild(10).GetComponent<Button>();
@@ -33,9 +42,9 @@ public class PasswordUIManager {
     }
 
     public void buttonClick(int num) {
-        if (ScriptManager.getInstance().isPlaying) return;
         txtPassword.text += num;
         if(txtPassword.text.Length > 3) {
+            ScriptManager.getInstance().isPlaying = false;
             int input = int.Parse(txtPassword.text);
 
             if(password == input) {
@@ -47,4 +56,12 @@ public class PasswordUIManager {
             MonoBehaviour.Destroy(window);
         }
     } //buttonClick
+
+    public class PasswordUI_wait : MonoBehaviour {
+        public IEnumerator waitScript(wake w) {
+            yield return new WaitUntil(() => !ScriptManager.getInstance().isPlaying);
+            w();
+            yield break;
+        }
+    }
 }
